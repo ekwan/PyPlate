5fe23eaeace78cb9ca70d797d1785eeea2ec9c98
James Marvin <47727350+JMarvi3@users.noreply.github.com>
2024-04-06 12:12:11 -0400

    Updated version numbers

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = '2024, Eugene Kwan and James Marvin'
 author = 'Eugene Kwan and James Marvin'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1'
+release = '0.4.1'
 
 # -- General configuration ---------------------------------------------------
 
